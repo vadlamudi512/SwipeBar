4c865e3f6f2e37eabc4ca8a13b0de5b83af38904
vadlamudi512 <venkateshvadlamudi512@gmail.com>
2023-01-30 20:30:08 +0530

    Animated swipebutton

---
+++ App.js
@@ -0,0 +1,35 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import SwipeButton from './src/SwipeButton';
+const App = () => {
+  const [toggleState, setToggleState] = useState(false);
+
+  const handleToggle = (value) => setToggleState(value);
+  return (
+    <SafeAreaView>
+      <View
+        style={[
+          styles.root,
+          { backgroundColor: toggleState ? '#222' : '#ebedee' },
+        ]}>
+        <GestureHandlerRootView>
+          <SwipeButton onToggle={handleToggle} />
+        </GestureHandlerRootView>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
